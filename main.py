f54932af65bd12c2b3403c2021d215b92e9326e4
generic-github-user <40661852+generic-github-user@users.noreply.github.com>
2021-04-15 09:43:18 -0400

    add base pattern generation

--- main.py
+++ main.py
@@ -1,3 +1,13 @@
 import numpy as np
 import matplotlib.pyplot as plt
 import random
+
+width = 100
+height = 100
+
+base = np.random.uniform(0, 1, [width, height, 1])
+base = np.zeros([width, height, 1])
+for i, y in enumerate(base):
+    for j, x in enumerate(y):
+        base[i][j] = i+j
+# base = np.ones([30, 30, 1])
