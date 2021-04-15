b42f33ea35bbb29579d2ac50703f5d2ab84b96e8
generic-github-user <40661852+generic-github-user@users.noreply.github.com>
2021-04-15 09:43:47 -0400

    add separate x and y bases (currently linear functions that are used as the inputs to the filter stacks)

--- main.py
+++ main.py
@@ -11,3 +11,19 @@ for i, y in enumerate(base):
     for j, x in enumerate(y):
         base[i][j] = i+j
 # base = np.ones([30, 30, 1])
+
+
+# bases = [
+#     np.linspace([0,0],[1,0],num=width,axis=0),
+#     np.linspace([0,0],[0,1],num=height,axis=1)
+# ]
+
+bases_per_axis = 2
+x = np.linspace(0, 1, width)
+y = np.linspace(0, 1, height)
+bases = []
+for i in range(bases_per_axis):
+    for r in np.meshgrid(x, y):
+        bases.append(r + np.random.uniform(0, 0.1, [width, height]))
+
+print([b.shape for b in bases])
